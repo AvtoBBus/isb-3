4242821b7ba090fd1bb6b185dbb79bde5fa839e6
AvtoBBus <113329450+AvtoBBus@users.noreply.github.com>
2023-05-15 21:44:25 +0400

    Fixed version
    -Изначальный класс разбит на 3 класса, каждый из которых, выполняет отдельные "сценарии"
    -Добавлено логирование
    -Добавлены докстринги
    -Исправлены мелкие недочёты
    -Добавлена обработка исключений при открытии файлов
    -settings вынесено в отдельный модуль config.py и импортируется в изначальный как константа

---
+++ config.py
@@ -0,0 +1,8 @@
+SETTINGS = {
+    'initial_file': 'top_secret/initial_file.txt',
+    'encrypted_file': 'top_secret/encrypted_file.txt',
+    'decrypted_file': 'top_secret/decrypted_file.txt',
+    'symmetric_key': 'top_secret/symmetric_key.txt',
+    'public_key': 'top_secret/ultra_secret/public_key.pem',
+    'secret_key': 'top_secret/ultra_secret/secret_key.pem',
+}
--- crypto.py
+++ crypto.py
@@ -1,62 +1,104 @@
 import argparse
-import binascii
 import json
 import os
-from pprint import pprint as pp
+import logging
 from cryptography.hazmat.primitives import serialization, hashes, padding as prim_padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding as asy_padding
 from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
 import create_initial_file as cif
+import config as cf
 
 
-class MyClass():
-    iv = bytes(0)
+class Generator():
     setting_path = ""
 
-    def __init__(self):
-        self.setting_path = "settings.json"
-        settings = {
-            'initial_file': 'top_secret/initial_file.txt',
-            'encrypted_file': 'top_secret/encrypted_file.txt',
-            'decrypted_file': 'top_secret/decrypted_file.txt',
-            'symmetric_key': 'top_secret/symmetric_key.txt',
-            'public_key': 'top_secret/ultra_secret/public_key.pem',
-            'secret_key': 'top_secret/ultra_secret/secret_key.pem',
-        }
-        with open(self.setting_path, 'w') as fp:
-            json.dump(settings, fp)
+    def __init__(self, setting_path: str):
+        '''
+        args:
+            setting_path : str - путь до файла с "настройками"
+        '''
+        self.setting_path = setting_path
+        with open(setting_path, 'w') as fp:
+            json.dump(cf.SETTINGS, fp)
 
     def generation(self) -> None:
-        with open('settings.json') as json_file:
-            json_data = json.load(json_file)
+        '''
+        Метод генерации симметричного и асимметричного ключа с последующим сохранением их в файлы
+        '''
+        try:
+            with open(self.setting_path) as json_file:
+                json_data = json.load(json_file)
+        except FileNotFoundError:
+            logging.error(f"{self.setting_path} not found")
         keys = rsa.generate_private_key(
             public_exponent=65537,
             key_size=2048
         )
+        logging.info("===Start generate keys===\n")
         cif.create_file(json_data['initial_file'])
+        logging.info("Initial file create\n")
         private_key = keys
         public_key = keys.public_key()
-        with open(json_data['public_key'], 'wb') as public_out:
-            public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
-                                                     format=serialization.PublicFormat.SubjectPublicKeyInfo))
-        with open(json_data['secret_key'], 'wb') as private_out:
-            private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
-                                                        format=serialization.PrivateFormat.TraditionalOpenSSL,
-                                                        encryption_algorithm=serialization.NoEncryption()))
+        try:
+            with open(json_data['public_key'], 'wb') as public_out:
+                public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                         format=serialization.PublicFormat.SubjectPublicKeyInfo))
+            logging.info("Public key create\n")
+        except FileNotFoundError:
+            logging.error(f"{json_data['public_key']} not found")
+        try:
+            with open(json_data['secret_key'], 'wb') as private_out:
+                private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                                            format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                                            encryption_algorithm=serialization.NoEncryption()))
+            logging.info("Secret key create\n")
+        except FileNotFoundError:
+            logging.error(f"{json_data['secret_key']} not found")
         c_symmetric_key = public_key.encrypt(os.urandom(16), asy_padding.OAEP(
             mgf=asy_padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
-        with open(json_data['symmetric_key'], 'wb') as simmetric_out:
-            simmetric_out.write(c_symmetric_key)
+        try:
+            with open(json_data['symmetric_key'], 'wb') as simmetric_out:
+                simmetric_out.write(c_symmetric_key)
+            logging.info("Symmetric key create\n")
+        except FileNotFoundError:
+            logging.error(f"{json_data['symmetric_key']} not found")
+        logging.info("===Finish generate keys===\n")
+
+
+class Encryptor():
+    setting_path = ""
+
+    def __init__(self, setting_path: str):
+        '''
+        args:
+            setting_path : str - путь до файла с "настройками"
+        '''
+        self.setting_path = setting_path
+        with open(setting_path, 'w') as fp:
+            json.dump(cf.SETTINGS, fp)
 
     def encryption(self) -> None:
-        with open('settings.json') as json_file:
-            json_data = json.load(json_file)
-        with open(json_data['secret_key'], 'rb') as pem_in:
-            private_key = pem_in.read()
+        '''
+        Метод шифрует данные в initial_file.txt, используя симметричный и асимметричный ключи, затем сохраняет результат в encrypted_file.txt
+        '''
+        logging.info("===Start encrypted===\n")
+        try:
+            with open(self.setting_path) as json_file:
+                json_data = json.load(json_file)
+        except FileNotFoundError:
+            logging.error(f"{self.setting_path} not found")
+        try:
+            with open(json_data['secret_key'], 'rb') as pem_in:
+                private_key = pem_in.read()
+        except FileNotFoundError:
+            logging.error(f"{json_data['secret_key']} not found")
         d_private_key = load_pem_private_key(private_key, password=None,)
-        with open(json_data['symmetric_key'], 'rb') as pem_in:
-            enc_symmetric_key = pem_in.read()
+        try:
+            with open(json_data['symmetric_key'], 'rb') as pem_in:
+                enc_symmetric_key = pem_in.read()
+        except FileNotFoundError:
+            logging.error(f"{json_data['symmetric_key']} not found")
         symmetric_key = d_private_key.decrypt(enc_symmetric_key, asy_padding.OAEP(
             mgf=asy_padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
         padder = prim_padding.ANSIX923(algorithms.SEED.block_size).padder()
@@ -64,57 +106,96 @@ class MyClass():
         cipher = Cipher(algorithms.SEED(symmetric_key),
                         modes.CBC(iv))
         encryptor = cipher.encryptor()
-        with open(json_data['initial_file'], 'rb') as i_f, open(json_data['encrypted_file'], 'wb') as e_f:
-            e_f.write(iv)
-            padded_block = bytes("", encoding="UTF-8")
-            while block := i_f.read(128):
-                padded_block += padder.update(block)
-            e_f.write(encryptor.update(padded_block) +
-                      encryptor.update(padder.finalize()) +
-                      encryptor.finalize())
+        try:
+            with open(json_data['initial_file'], 'rb') as i_f, open(json_data['encrypted_file'], 'wb') as e_f:
+                e_f.write(iv)
+                padded_block = bytes("", encoding="UTF-8")
+                while block := i_f.read(128):
+                    padded_block += padder.update(block)
+                e_f.write(encryptor.update(padded_block) +
+                          encryptor.update(padder.finalize()) +
+                          encryptor.finalize())
+        except FileNotFoundError:
+            logging.error(f"{json_data['initial_file']} not found" if not os.path.isfile(
+                json_data['initial_file']) else f"{json_data['encrypted_file']} not found")
+        logging.info(
+            f"===Finish encrypted===\nResult in file {json_data['encrypted_file']}")
+
+
+class Decryptor():
+    setting_path = ""
+
+    def __init__(self, setting_path: str):
+        '''
+        args:
+            setting_path - путь до файла с "настройками"
+        '''
+        self.setting_path = setting_path
+        with open(setting_path, 'w') as fp:
+            json.dump(cf.SETTINGS, fp)
 
     def decryption(self) -> None:
-        with open('settings.json') as json_file:
-            json_data = json.load(json_file)
-        with open(json_data['secret_key'], 'rb') as pem_in:
-            private_key = pem_in.read()
+        '''
+        Метод дешифрует данные в encrypted_file.txt, используя симметричный и асимметричный ключи, затем сохраняет результат в decrypted_file.txt
+        '''
+        logging.info("===Start decrypted===\n")
+        try:
+            with open(self.setting_path) as json_file:
+                json_data = json.load(json_file)
+        except FileNotFoundError:
+            logging.error(f"{self.setting_path} not found")
+        try:
+            with open(json_data['secret_key'], 'rb') as pem_in:
+                private_key = pem_in.read()
+        except FileNotFoundError:
+            logging.error(f"{json_data['secret_key']} not found")
         d_private_key = load_pem_private_key(private_key, password=None,)
-        with open(json_data['symmetric_key'], 'rb') as pem_in:
-            enc_symmetric_key = pem_in.read()
+        try:
+            with open(json_data['symmetric_key'], 'rb') as pem_in:
+                enc_symmetric_key = pem_in.read()
+        except FileNotFoundError:
+            logging.error(f"{json_data['symmetric_key']} not found")
         symmetric_key = d_private_key.decrypt(enc_symmetric_key, asy_padding.OAEP(
             mgf=asy_padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
-        with open(json_data['encrypted_file'], 'rb') as f_in, open(json_data['decrypted_file'], 'wb') as f_out:
-            iv = f_in.read(16)
-            cipher = Cipher(algorithms.SEED(symmetric_key),
-                            modes.CBC(iv))
-            decryptor = cipher.decryptor()
-            unpadder = prim_padding.ANSIX923(
-                algorithms.SEED.block_size).unpadder()
-            unpadded_block = bytes("", encoding="UTF-8")
-            while block := f_in.read(128):
-                unpadded_block += decryptor.update(block)
-            f_out.write(unpadder.update(unpadded_block) +
-                        unpadder.update(decryptor.finalize()) +
-                        unpadder.finalize())
+        try:
+            with open(json_data['encrypted_file'], 'rb') as f_in, open(json_data['decrypted_file'], 'wb') as f_out:
+                iv = f_in.read(16)
+                cipher = Cipher(algorithms.SEED(symmetric_key),
+                                modes.CBC(iv))
+                decryptor = cipher.decryptor()
+                unpadder = prim_padding.ANSIX923(
+                    algorithms.SEED.block_size).unpadder()
+                unpadded_block = bytes("", encoding="UTF-8")
+                while block := f_in.read(128):
+                    unpadded_block += decryptor.update(block)
+                f_out.write(unpadder.update(unpadded_block) +
+                            unpadder.update(decryptor.finalize()) +
+                            unpadder.finalize())
+        except FileNotFoundError:
+            logging.error(f"{json_data['encrypted_file']} not found" if not os.path.isfile(
+                json_data['encrypted_file']) else f"{json_data['decrypted_file']} not found")
+        logging.info(
+            f"===Finish encrypted===\nResult in file {json_data['decrypted_file']}")
 
 
-mc = MyClass()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
 
-parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group(required=True)
-
-group.add_argument('-gen', '--generation',
-                   help='Запускает режим генерации ключей')
-group.add_argument('-enc', '--encryption',
-                   help='Запускает режим шифрования, результат шифрования помещается в файл c_text.txt')
-group.add_argument('-dec', '--decryption',
-                   help='Запускает режим дешифрования, на вход подаётся путь то .txt файла с зашифрованным текстом')
-args = parser.parse_args()
-
-
-if args.generation is not None:
-    mc.generation()
-if args.encryption is not None:
-    mc.encryption()
-if args.decryption is not None:
-    mc.decryption()
+    group.add_argument('-gen', '--generation',
+                       help='Запускает режим генерации ключей')
+    group.add_argument('-enc', '--encryption',
+                       help='Запускает режим шифрования, результат шифрования помещается в файл c_text.txt')
+    group.add_argument('-dec', '--decryption',
+                       help='Запускает режим дешифрования, на вход подаётся путь то .txt файла с зашифрованным текстом')
+    args = parser.parse_args()
+    setting_path = "settings.json"
+    if args.generation:
+        generator = Generator(setting_path)
+        generator.generation()
+    if args.encryption:
+        encryptor = Encryptor(setting_path)
+        encryptor.encryption()
+    if args.decryption:
+        decryptor = Decryptor(setting_path)
+        decryptor.decryption()
